23839f79cb07c925edfafa635f38346471a0f648
이임충 <icll@naver.com>
2025-01-16 15:54:43 +0900

    feat: vue + bootstrap 적용

--- vuefrontend/src/main.js
+++ vuefrontend/src/main.js
@@ -1,18 +1,23 @@
-import './assets/main.css'
+import "./assets/main.css";
 
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { createBootstrap } from "bootstrap-vue-next";
+import App from "./App.vue";
 
 // FontAwesomeIcon 설정
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { faR } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { faR } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
 
-library.add(fas)
-library.add(faR)
+// Add the necessary CSS
+import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap-vue-next/dist/bootstrap-vue-next.css";
 
-const app = createApp(App)
-app.component('font-awesome-icon', FontAwesomeIcon)
+library.add(fas);
+library.add(faR);
 
-app.mount('#app')
+const app = createApp(App);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.use(createBootstrap()) // Important
+app.mount("#app");
--- vuefrontend/vite.config.js
+++ vuefrontend/vite.config.js
@@ -1,17 +1,24 @@
-import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-vue';
+import { defineConfig } from "vite";
+import plugin from "@vitejs/plugin-vue";
+import Components from "unplugin-vue-components/vite";
+import { BootstrapVueNextResolver } from "bootstrap-vue-next";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
-    server: {
-        port: 50142,
-        proxy: {
-            '/api': {
-                target: 'https://localhost:44363', // ASP.NET MVC5 서버 주소
-                changeOrigin: true,
-                secure: false,
-            },
-        },
-    }
-})
+  plugins: [
+    plugin(),
+    Components({
+      resolvers: [BootstrapVueNextResolver()],
+    }),
+  ],
+  server: {
+    port: 50142,
+    proxy: {
+      "/api": {
+        target: "https://localhost:44363", // ASP.NET MVC5 서버 주소
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
